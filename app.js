166f82e203702a1381c2fb96605c1f8394131a61
Anket17 <anketnemlekar17@gmail.com>
2020-09-23 16:43:51 +0530

    asdsasda

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://Anket17:Bboyload1998@cluster0.kbiua.gcp.mongodb.net/VeganCart?retryWrites=true&w=majority',{useNewUrlParser : true,useUnifiedTopology: true},()=>{
+    console.log('successfully connected to database');
+});
+
+const userRouter = require('./routes/User');
+app.use('/user',userRouter);
+
+app.listen(5000,()=>{
+    console.log('express server started');
+});
